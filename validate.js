2a3e7aa10b696f3c4050554d97a322c03782bf68
FND <fnd@fnd-oda.(none)>
2011-10-31 14:55:12 +0100

    removed trailing whitespace
    as recommended by JSLint

--- validate.js
+++ validate.js
@@ -29,14 +29,14 @@
             integer: 'The %s field must contain an integer.'
         },
         callback: function(errors) {
-        
+
         }
     };
-    
+
     /*
      * Define the regular expressions that will be used
      */
-    
+
     var ruleRegex = /^(.+)\[(.+)\]$/,
         numericRegex = /^[0-9]+$/,
         integerRegex = /^\-?[0-9]+$/,
@@ -45,7 +45,7 @@
         alphaRegex = /^[a-z]+$/i,
         alphaNumericRegex = /^[a-z0-9]+$/i,
         alphaDashRegex = /^[a-z0-9_-]+$/i;
-        
+
     /*
      * The exposed public object to validate a form:
      *
@@ -67,19 +67,19 @@
         this.form = document.forms[formName] || {};
         this.messages = {};
         this.handlers = {};
-        
+
         for (var i = 0, fieldLength = fields.length; i < fieldLength; i++) {
             var field = fields[i];
-        
+
             // If passed in incorrectly, we need to skip the field.
             if (!field.name || !field.rules) {
                 continue;
             }
-        
+
             /*
              * Build the master fields array that has all the information needed to validate
              */
-             
+
             this.fields[field.name] = {
                 name: field.name,
                 display: field.display || field.name,
@@ -89,11 +89,11 @@
                 checked: null
             }
         }
-        
+
         /*
          * Attach an event callback for the form submission
          */
-        
+
         this.form.onsubmit = (function(that) {
             return function(event) {
                 try {
@@ -102,64 +102,64 @@
             }
         })(this);
     };
-    
+
     /*
      * @public
      * Sets a custom message for one of the rules
      */
-    
+
     FormValidator.prototype.setMessage = function(rule, message) {
         this.messages[rule] = message;
-        
+
         // return this for chaining
         return this;
     };
-    
+
     /*
      * @public
      * Registers a callback for a custom rule (i.e. callback_username_check)
      */
-    
+
     FormValidator.prototype.registerCallback = function(name, handler) {
         if (name && typeof name === 'string' && handler && typeof handler === 'function') {
             this.handlers[name] = handler;
         }
-        
+
         // return this for chaining
         return this;
     };
-    
+
     /*
      * @private
      * Runs the validation when the form is submitted.
      */
-    
+
     FormValidator.prototype._validateForm = function(event) {
         this.errors = [];
-    
+
         for (var key in this.fields) {
             if (this.fields.hasOwnProperty(key)) {
                 var field = this.fields[key] || {},
                     element = this.form[field.name];
-            
+
                 if (element && element !== undefined) {
                     field.type = element.type;
                     field.value = element.value;
                     field.checked = element.checked;
                 }
-                
+
                 /*
                  * Run through the rules for each field.
                  */
-                
+
                 this._validateField(field);
             }
         }
-        
+
         if (typeof this.callback === 'function') {
             this.callback(this.errors, event);
         }
-        
+
         if (this.errors.length > 0) {
             if (event && event.preventDefault) {
                 event.preventDefault();
@@ -168,30 +168,30 @@
                 return false;
             }
         }
-        
+
         return true;
     };
-    
+
     /*
      * @private
      * Looks at the fields value and evaluates it against the given rules
      */
-    
+
     FormValidator.prototype._validateField = function(field) {
         var rules = field.rules.split('|');
-        
+
         /*
          * If the value is null and not required, we don't need to run through validation
          */
-         
+
         if (field.rules.indexOf('required') === -1 && (!field.value || field.value === '' || field.value === undefined)) {
             return;
         }
-        
+
         /*
          * Run through the rules and execute the validation methods as needed
          */
-        
+
         for (var i = 0, ruleLength = rules.length; i < ruleLength; i++) {
             var method = rules[i],
                 param = null,
@@ -205,11 +205,11 @@
                 method = parts[1];
                 param = parts[2];
             }
-            
+
             /*
              * If the hook is defined, run it to find any validation errors
              */
-            
+
             if (typeof this._hooks[method] === 'function') {
                 if (!this._hooks[method].apply(this, [field, param])) {
                     failed = true;
@@ -217,92 +217,92 @@
             } else if (method.substring(0, 9) === 'callback_') {
                 // Custom method. Execute the handler if it was registered
                 method = method.substring(9, method.length);
-                
+
                 if (typeof this.handlers[method] === 'function') {
                     if (this.handlers[method].apply(this, [field.value]) === false) {
                         failed = true;
                     }
                 }
             }
-            
+
             /*
              * If the hook failed, add a message to the errors array
              */
-             
+
             if (failed) {
                 // Make sure we have a message for this rule
                 var source = this.messages[method] || defaults.messages[method];
-                
+
                 if (source) {
                     var message = source.replace('%s', field.display);
-                    
+
                     if (param) {
                         message = message.replace('%s', (this.fields[param]) ? this.fields[param].display : param);
                     }
-                    
+
                     this.errors.push(message);
                 } else {
                     this.errors.push('An error has occurred with the ' + field.display + ' field.');
                 }
-                
+
                 // Break out so as to not spam with validation errors (i.e. required and valid_email)
                 break;
             }
         }
     };
-    
+
     /*
      * @private
      * Object containing all of the validation hooks
      */
-    
+
     FormValidator.prototype._hooks = {
         required: function(field) {
             var value = field.value;
-            
+
             if (field.type === 'checkbox') {
                 return (field.checked === true);
             }
-        
+
             return (value !== null && value !== '');
         },
-        
+
         matches: function(field, matchName) {
             if (el = this.form[matchName]) {
                 return field.value === el.value;
             }
-            
+
             return false;
         },
-        
+
         valid_email: function(field) {
             return emailRegex.test(field.value);
         },
-        
+
         min_length: function(field, length) {
             if (!numericRegex.test(length)) {
                 return false;
             }
-            
+
             return (field.value.length >= length);
         },
-        
+
         max_length: function(field, length) {
             if (!numericRegex.test(length)) {
                 return false;
             }
-            
+
             return (field.value.length <= length);
         },
-        
+
         exact_length: function(field, length) {
             if (!numericRegex.test(length)) {
                 return false;
             }
-            
+
             return (field.value.length == length);
         },
-        
+
         greater_than: function(field, param) {
             if (!decimalRegex.test(field.value)) {
                 return false;
@@ -310,31 +310,31 @@
 
             return (parseFloat(field.value) > parseFloat(param));
         },
-        
+
         less_than: function(field, param) {
             if (!decimalRegex.test(field.value)) {
                 return false;
             }
-            
+
             return (parseFloat(field.value) < parseFloat(param));
         },
-        
+
         alpha: function(field) {
             return (alphaRegex.test(field.value));
         },
-        
+
         alpha_numeric: function(field) {
             return (alphaNumericRegex.test(field.value));
         },
-        
+
         alpha_dash: function(field) {
             return (alphaDashRegex.test(field.value));
         },
-        
+
         numeric: function(field) {
             return (decimalRegex.test(field.value));
         },
-        
+
         integer: function(field) {
             return (integerRegex.test(field.value));
         }
