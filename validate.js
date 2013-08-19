b1d592eeda7af6eed777eba3681e873ac73d273e
Rick Harrison <rick.harrison@me.com>
2013-08-18 17:06:51 -0700

    Updated the version number.

--- validate.js
+++ validate.js
@@ -1,5 +1,5 @@
 /*
- * validate.js 1.2.2
+ * validate.js 1.3
  * Copyright (c) 2011 Rick Harrison, http://rickharrison.me
  * validate.js is open sourced under the MIT license.
  * Portions of validate.js are inspired by CodeIgniter.
